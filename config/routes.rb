55740f0982c8af66518dba49f8e21ab99da4ca48
YukikoNiwa <112695050+iamyukiko@users.noreply.github.com>
2022-09-22 10:53:48 +0000

    [Update]created userpage

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ root to: "homes#top"
 devise_for :users
 get '/homes/about' => 'homes#about', as: 'about'
 resources :books, only: [:new, :index, :show, :create]
-resources :users, only:[:show, :edit]
+resources :users, only:[:show, :edit, :update]
 end
